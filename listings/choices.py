b3e2f618caea68152867a029f0d374b7ccb9da85
unknown <venkatesh.kandasamy@gmail.com>
2020-10-03 17:12:29 +0530

    Initial commit

---
+++ listings/choices.py
@@ -0,0 +1,31 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7
+}
+
+price_choices = {
+    '100000': '₹1,00,000',
+    '200000': '₹2,00000',
+    '300000': '₹300000',
+    '400000': '₹400000',
+    '500000': '₹500000',
+    '600000': '₹600000',
+    '700000': '₹700000',
+    '800000': '₹800000',
+}
+
+state_choices = {
+    'Tamil Nadu': 'Tamil Nadu',
+    'Telangana': 'Telangana',
+    'Kerala': 'Kerala',
+    'Karnataka': 'Karnataka',
+    'Andhra Pradesh': 'Andhra Pradesh',
+    'Pondicherry': 'Pondicherry',
+    'Maharashtra': 'Maharashtra',
+    'Delhi': 'Delhi',
+}
